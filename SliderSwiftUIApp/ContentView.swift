f84a900e4f68b6d7a51801dbe221d3d6b4e44ae8
MatveenkovNikita <matveenkoff@gmail.com>
2021-06-07 03:20:43 +0300

    SwiftUISliderApp

--- SliderSwiftUIApp/ContentView.swift
+++ SliderSwiftUIApp/ContentView.swift
@@ -6,75 +6,90 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State private var redValue = 50.0
+    @State private var blueValue = 50.0
+    @State private var greenValue = 50.0
+    
+    @State private var redValueLabel = ""
+    @State private var blueValueLabel = ""
+    @State private var greenValueLabel = ""
+    
+    @State private var displayedLabel = ""
+    @State private var alertPresented = false
+    
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+        
+        VStack(spacing: -30){
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 300, height: 100, alignment: .center)
+                .padding(150)
+                .foregroundColor(Color.init(UIColor.init(displayP3Red: CGFloat(redValue)/255, green: CGFloat(greenValue)/255, blue: CGFloat(blueValue)/255, alpha: 1)) )
+                
+            HStack{
+                Text("\(lround(redValue))")
+                    .frame(width: 35, height: 35)
+                Slider(value: $redValue, in: 0...255, step: 1)
+                    .colorMultiply(.red)
+                    .frame(width: 290, height: 100)
+                TextField("\(lround(redValue))", text: $redValueLabel, onCommit:  {
+                    displayedLabel = String(redValue)
+                    redValue = checkValue(value: redValueLabel)
+                    redValueLabel = ""
+                })
+                .alert(isPresented: $alertPresented, content: {
+                    Alert(title: Text("Wrong format!"), message: Text("Enter number in 0 to 255"), dismissButton: Alert.Button.cancel())
+                })
             }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
 
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
+            HStack{
+                Text("\(lround(blueValue))")
+                    .frame(width: 35, height: 100)
+                Slider(value: $blueValue, in: 0...255, step: 1)
+                    .colorMultiply(.blue)
+                    .frame(width: 290, height: 100)
+                TextField("\(lround(blueValue))", text: $blueValueLabel, onCommit:  {
+                    displayedLabel = String(blueValue)
+                    blueValue = checkValue(value: blueValueLabel)
+                    blueValueLabel = ""
+                })
+                .alert(isPresented: $alertPresented, content: {
+                    Alert(title: Text("Wrong format!"), message: Text("Enter number in 0 to 255"), dismissButton: Alert.Button.cancel())
+                })
+            }
+
+            HStack{
+                Text("\(lround(greenValue))")
+                    .frame(width: 35, height: 100)
+                Slider(value: $greenValue, in: 0...255, step: 1)
+                    .colorMultiply(.green)
+                    .frame(width: 290, height: 100)
+                TextField("\(lround(greenValue))", text: $greenValueLabel, onCommit:  {
+                    displayedLabel = String(greenValue)
+                    greenValue = checkValue(value: greenValueLabel)
+                    greenValueLabel = ""
+                })
+                .alert(isPresented: $alertPresented, content: {
+                    Alert(title: Text("Wrong format!"), message: Text("Enter number in 0 to 255"), dismissButton: Alert.Button.cancel())
+                })
             }
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+    
+    private func checkValue(value: String) -> Double {
+        if let newValue = Double(value),
+           Double(value)! >= 0,
+           Double(value)! <= 255 {
+            return  newValue
         }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        alertPresented.toggle()
+        return Double(displayedLabel)!
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
